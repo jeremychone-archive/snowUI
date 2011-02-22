dc4e48de752a334d507212eda5c4dc24ec1d5a0e
jeremychone <jeremy.chone@gmail.com>
2011-02-22 14:44:13 -0800

    + added snow.gtx(canvasContext) context chaining wrapper

--- src/js/snow.core.js
+++ src/js/snow.core.js
@@ -506,5 +506,77 @@ snow.ua = (function(){
 })();
 
 // ------ /snow.ua (User Agent) ------ //
-// ---------------------------------- //
+// ----------------------------------- //
 
+// ----------------------- //
+// ------ snow.gtx ------- //
+
+//from: https://developer.mozilla.org/en/Code_snippets/Canvas
+(function(){
+    function Gtx(arg){
+		var ctx = arg;
+    	if ($.isFunction(arg.getContext)){
+			ctx = arg.getContext('2d');
+		}
+        
+		// This allow to use the new or just the method
+		if (!(this instanceof Gtx)) {
+            return new Gtx(ctx);
+        }
+		
+        this.context = this.ctx = ctx;
+		
+		//build the prototype methods on first demand 
+        if (!Gtx.prototype.arc) {
+			//Note: not sure the this, this.ctx are needed here. 
+            Gtx.setup.call(this, this.ctx);
+        }
+    }
+    Gtx.setup = function(){
+        var methods = ['arc', 'arcTo', 'beginPath', 'bezierCurveTo', 'clearRect', 'clip', 'closePath', 'drawImage', 'fill', 'fillRect', 'fillText', 'lineTo', 'moveTo', 'quadraticCurveTo', 'rect', 'restore', 'rotate', 'save', 'scale', 'setTransform', 'stroke', 'strokeRect', 'strokeText', 'transform', 'translate'];
+        
+        var getterMethods = ['createPattern', 'drawFocusRing', 'isPointInPath', 'measureText', // drawFocusRing not currently supported
+        // The following might instead be wrapped to be able to chain their child objects
+        'createImageData', 'createLinearGradient', 'createRadialGradient', 'getImageData', 'putImageData'];
+        
+        var props = ['canvas', 'fillStyle', 'font', 'globalAlpha', 'globalCompositeOperation', 'lineCap', 'lineJoin', 'lineWidth', 'miterLimit', 'shadowOffsetX', 'shadowOffsetY', 'shadowBlur', 'shadowColor', 'strokeStyle', 'textAlign', 'textBaseline'];
+        
+        var gmethl, propl;
+        for (var i = 0, methl = methods.length; i < methl; i++) {
+            var m = methods[i];
+            Gtx.prototype[m] = (function(m){
+                return function(){
+                    this.ctx[m].apply(this.ctx, arguments);
+                    return this;
+                };
+            }(m));
+        }
+        
+        for (i = 0, gmethl = getterMethods.length; i < gmethl; i++) {
+            var gm = getterMethods[i];
+            Gtx.prototype[gm] = (function(gm){
+                return function(){
+                    return this.ctx[gm].apply(this.ctx, arguments);
+                };
+            }(gm));
+        }
+        
+        for (i = 0, propl = props.length; i < propl; i++) {
+            var p = props[i];
+            Gtx.prototype[p] = (function(p){
+                return function(value){
+                    if (typeof value === 'undefined') {
+                        return this.ctx[p];
+                    }
+                    this.ctx[p] = value;
+                    return this;
+                };
+            }(p));
+        }
+    };
+	
+	snow.gtx = Gtx;
+})();
+
+// ------ /snow.gtx ------- //
+// ----------------------- //
